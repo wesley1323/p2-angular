3eeda842aca8065b14891ff207e159d6e17800bf
wesley1323 <wesleynascimentosillva@gmail.com>
2022-06-17 18:52:13 -0300

    4 files modified

--- src/app/wesley-home/wesley-home.component.ts
+++ src/app/wesley-home/wesley-home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WesleyTasksService } from '../wesley-tasks.service';
 
 @Component({
   selector: 'app-wesley-home',
   templateUrl: './wesley-home.component.html',
-  styleUrls: ['./wesley-home.component.css']
+  styleUrls: ['./wesley-home.component.css'],
 })
 export class WesleyHomeComponent implements OnInit {
+  constructor(public listaTasks: WesleyTasksService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/wesley-tasks/wesley-tasks.component.ts
+++ src/app/wesley-tasks/wesley-tasks.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WesleyTasksService } from '../wesley-tasks.service';
 
 @Component({
   selector: 'app-wesley-tasks',
   templateUrl: './wesley-tasks.component.html',
-  styleUrls: ['./wesley-tasks.component.css']
+  styleUrls: ['./wesley-tasks.component.css'],
 })
 export class WesleyTasksComponent implements OnInit {
+  constructor(public listaTasks: WesleyTasksService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
