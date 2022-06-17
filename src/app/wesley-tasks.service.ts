22e2b7bd14ff25a1dc08f7cac9b95da49a85c800
wesley1323 <wesleynascimentosillva@gmail.com>
2022-06-17 18:37:55 -0300

    1 file modified

--- src/app/wesley-tasks.service.ts
+++ src/app/wesley-tasks.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
-
+interface Task {
+  Nome: string;
+}
 @Injectable()
 export class WesleyTasksService {
+  listaTask: Array<Task> = [];
+  constructor() {
+    if (localStorage.getItem('tarefas')) {
+      this.listaTask = JSON.parse(localStorage.getItem('tarefas'));
+    } else {
+      localStorage.setitem('tarefas', JSON.stringify([]));
+    }
+  }
+  addTask(nome: string) {
+    if (nome !== '') {
+      this.listaTask.push({ nome: nome });
+      localStorage.setitem('tarefas', JSON.stringify(this.listaTask));
+    }
+  }
 
-  constructor() { }
+  removeTask(index: number) {
+    if (this.listaTask != null) {
+      this.listaTask.splice(index, 1);
+      localStorage.setItem('tarefas', JSON.stringify(this.listaTask));
+    }
+  }
 
+  getTasks() {
+    if (this.listaTask != null) {
+      this.listaTask = JSON.parse(localStorage.getItem('tarefas'));
+    }
+  }
 }
